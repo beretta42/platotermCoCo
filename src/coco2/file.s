088416f62f48d44ff3f37910bceac106ff2d6a03
Brett Gordon <beretta42@gmail.com>
2018-11-27 12:12:26 -0500

    tidy up file handling interaction - use hook more properly
    unfortunately BASIC's open hook is hard to use from the RAM vectors, as
    it starts noodling with evaluating BASIC source, etc...

--- src/coco2/file.s
+++ src/coco2/file.s
@@ -20,6 +20,7 @@ _open_ll:
 	;; this DECB vectors should be grokked from the BASIC
 	;; RAM vectors in DP. Don't assume decb1.1 locations!
 	jsr	$c48d		; call 1.1 DECB's open
+open_ok:
 	ldx	#1		; return no error
 	puls	y,u,pc 
 open_err:
@@ -29,34 +30,58 @@ open_err:
 
 	;; read byte from file
 _read_ll:
-	jsr	$c5c4
+	sts	save
+	ldx	#read_err
+	stx	$192
+	ldx	#read_ok
+	pshs	x
+	jsr	$16a
+read_ok:
 	tst	<$70		; read BASIC's EOF flag
-	bne	eof
+	bne	read_eof
 	tfr	a,b
 	clra
 	tfr	d,x
 	rts
-eof:	ldx	#-1
+read_err:
+	lds	save
+	ldx	#-2
+	rts
+read_eof:
+	ldx	#-1
 	rts
 
 	;; write byte to file
 _write_ll:
+	sts	save
+	ldx	#write_err
+	stx	$192
+	ldx	#write_ok
+	pshs	x
 	exg	a,b
-	jsr	$cc24
+	jsr	$167
+write_ok:
 	ldx	#0
+	rts
+write_err:
+	lds	save
+	ldx	#-1
 	rts
 
 
 	;;
 _close_ll:	
 	pshs	y,u
-	sts	save
-	ldy	#close_err
+	sts	save		; remember stack frame
+	ldy	#close_err	; usurp any BASIC errors
 	sty	$192
-	jsr	$caf9
-	ldx	#0
+	ldx	#close_ok
+	pshs	x		; push return address twice BASIC will gobble it
+	jsr	$176		; call BASIC close ram hook
+close_ok:
+	ldx	#0		; closed ok!
 	puls	y,u,pc
 close_err:
-	lds	save
-	ldx	#-1
+	lds	save		; reset stack frame
+	ldx	#-1		; closed bad!
 	puls	y,u,pc
